d357257c5a3e1b2f40c3439b921ec543659581b6
JEFrady <jamie.e.halliday@gmail.com>
2018-08-02 19:57:51 -1000

    Made CSS less shit, can get all data from Reddit except age, and views. JS exploritory, not functional

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 let inspect = new XMLHttpRequest();
 inspect.addEventListener('load', function(res){
-    let target = res.currentTarget.response
-    console.log(target)
-
-
+    let target = JSON.parse(res.currentTarget.response).data.children
+    for (let i=0;i<target.length;i++) {
+        let post = target[i];
+        console.log("Post Title: " + post.data.title)
+        console.log("Post Author: " + post.data.author)
+        // console.log("Post Title: " + post.data.preview.images[0].source.url)
+        console.log(post.data)
+    }
 })
-inspect.open('GET', 'https://www.reddit.com/r/andeanmusic.json');
+inspect.open('GET', 'https://www.reddit.com/r/youseeingthisshit.json');
 inspect.send();
 
